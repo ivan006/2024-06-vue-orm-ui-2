ba17b7d32862a7d0401c05baf0413dd1af58ba40
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-06-07 23:30:35 +0200

    1

--- src/quick-list/SuperTableTable.vue
+++ src/quick-list/SuperTableTable.vue
@@ -1,160 +1,164 @@
 <template>
-    <div>
-      <!--<pre>{{flattenedHeadersHideMapField}}</pre>-->
-        <v-data-table-server
-            :mobile-breakpoint="0"
-            @click:row="clickRow"
-            :headers="flattenedHeadersHideMapField"
-            :items="items"
-            :itemsLength="+itemsLength"
-            :page="optionsComputed.page"
-            :items-per-page="optionsComputed.itemsPerPage"
-            @update:options="(e)=>{$emit('update:options', e)}"
-            v-model:sort-by="sortBy"
-        >
-            <template
-                v-for="(header, index) in flattenedHeaders"
-                v-slot:[`item.${header.key}`]="{ item }"
-                :key="index"
-            >
-                <template v-if="header.isChildOf">
-                    <div>
-                        <FormattedColumn
-                            :isTag="true"
-                            :header="header"
-                            :item="item[header.isChildOf.value]"
-                            :superOptions="superOptions"
-                        />
-                    </div>
-                </template>
-                <template v-else>
-                    <div>
-                        <FormattedColumn
-                            :header="header"
-                            :item="item"
-                            :superOptions="superOptions"
-                        />
-                    </div>
-                </template>
-            </template>
-        </v-data-table-server>
-    </div>
+  <div>
+    <!--<pre>{{flattenedHeadersHideMapField}}</pre>-->
+    <v-data-table-server
+        :mobile-breakpoint="0"
+        @click:row="clickRow"
+        :headers="flattenedHeadersHideMapField"
+        :items="items"
+        :itemsLength="+itemsLength"
+        :page="optionsComputed.page"
+        :items-per-page="optionsComputed.itemsPerPage"
+        @update:options="
+        (e) => {
+          $emit('update:options', e);
+        }
+      "
+        v-model:sort-by="sortBy"
+    >
+      <template
+          v-for="(header, index) in flattenedHeaders"
+          v-slot:[`item.${header.key}`]="{ item }"
+          :key="index"
+      >
+        <template v-if="header.isChildOf">
+          <div>
+            <FormattedColumn
+                :isTag="true"
+                :header="header"
+                :item="item[header.isChildOf.value]"
+                :superOptions="superOptions"
+            />
+          </div>
+        </template>
+        <template v-else>
+          <div>
+            <FormattedColumn
+                :header="header"
+                :item="item"
+                :superOptions="superOptions"
+            />
+          </div>
+        </template>
+      </template>
+    </v-data-table-server>
+  </div>
 </template>
 
 <script>
-import FormattedColumn from './FormattedColumn.vue'
+import FormattedColumn from "./FormattedColumn.vue";
 
 export default {
-    name: 'SuperTableTable',
-    components: { FormattedColumn },
+  name: "SuperTableTable",
+  components: { FormattedColumn },
 
-    props: {
-        items: {
-            type: Array,
-            default() {
-                return []
-            },
-        },
-        options: {
-            type: Object,
-            default() {
-                return {}
-            },
-        },
-        itemsLength: {
-            type: Number,
-            default() {
-                return null
-            },
-        },
-        superOptions: {
-          type: Object,
-          default() {
-            return {
-              headers: [],
-              modelFields: [],
-              displayMapField: false,
-              model: {},
-              canEdit: false,
-              currentParentRecord: {},
-            }
-          },
-        },
+  props: {
+    items: {
+      type: Array,
+      default() {
+        return [];
+      },
     },
-  data(){
-    return {
-      sortBy: []
-    }
+    options: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    itemsLength: {
+      type: Number,
+      default() {
+        return null;
+      },
+    },
+    superOptions: {
+      type: Object,
+      default() {
+        return {
+          headers: [],
+          modelFields: [],
+          displayMapField: false,
+          model: {},
+          canEdit: false,
+          currentParentRecord: {},
+        };
+      },
+    },
   },
-    computed: {
-        pKey() {
-          return this.superOptions.model.primaryKey
-        },
-        optionsComputed: {
-            get() {
-                return this.options
-            },
-            set(value) {
-                this.$emit('update:options', value)
-            },
-        },
-        flattenedHeaders() {
-            let result = []
-            for (const header of this.superOptions.headers) {
-                result.push(header)
-                if (header.headerChildren) {
-                    for (const childHeader of header.headerChildren) {
-                        result.push({
-                            // isChildOf: header,
-                            ...childHeader,
-                        })
-                    }
-                }
-            }
-            return result
-        },
-        flattenedHeadersHideMapField() {
-            let result = []
-            for (const header of this.flattenedHeaders) {
-                if (!this.superOptions.superOptions) {
-                    if (
-                        !header.usageType.startsWith('relLookupMapExtra') &&
-                        !header.usageType.startsWith('mapExtra')
-                    ) {
-                        result.push(header)
-                    }
-                } else {
-                  result.push(header)
-                }
-            }
-            return result
-        },
+  data() {
+    return {
+      sortBy: [],
+    };
+  },
+  computed: {
+    pKey() {
+      return this.superOptions.model.primaryKey;
     },
-    methods: {
-        updateOptions(e) {
-            this.$emit('updateOptions', e)
-        },
-        deleteItem(e) {
-            this.$emit('deleteItem', e)
-        },
-        editItem(e) {
-            this.$emit('editItem', e)
-        },
-        clickRow(e, row) {
-            this.$emit('clickRow', row.item)
-        },
+    optionsComputed: {
+      get() {
+        return this.options;
+      },
+      set(value) {
+        this.$emit("update:options", value);
+      },
     },
-    watch: {
-        'editItemData.showModal'(arg) {
-            if (!arg) {
-                this.editItemData.data = {}
-            }
-        },
+    flattenedHeaders() {
+      let result = [];
+      for (const header of this.superOptions.headers) {
+        result.push(header);
+        if (header.headerChildren) {
+          for (const childHeader of header.headerChildren) {
+            result.push({
+              // isChildOf: header,
+              ...childHeader,
+            });
+          }
+        }
+      }
+      return result;
     },
-  mounted(){
-    this.sortBy = [{ key: this.pKey, order: 'asc' }]
-  }
-}
+    flattenedHeadersHideMapField() {
+      let result = [];
+      for (const header of this.flattenedHeaders) {
+        if (!this.superOptions.superOptions) {
+          if (
+              !header.usageType.startsWith("relLookupMapExtra") &&
+              !header.usageType.startsWith("mapExtra")
+          ) {
+            result.push(header);
+          }
+        } else {
+          result.push(header);
+        }
+      }
+      return result;
+    },
+  },
+  methods: {
+    updateOptions(e) {
+      this.$emit("updateOptions", e);
+    },
+    deleteItem(e) {
+      this.$emit("deleteItem", e);
+    },
+    editItem(e) {
+      this.$emit("editItem", e);
+    },
+    clickRow(e, row) {
+      this.$emit("clickRow", row.item);
+    },
+  },
+  watch: {
+    "editItemData.showModal"(arg) {
+      if (!arg) {
+        this.editItemData.data = {};
+      }
+    },
+  },
+  mounted() {
+    this.sortBy = [{ key: this.pKey, order: "asc" }];
+  },
+};
 </script>
 
 <style scoped></style>
